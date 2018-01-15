db4905c82ef1fc083f1a641c303e16687351b9c8
sbotham <sbotham@arcnp.com>
2018-01-15 11:33:54 -0500

    1

---
+++ MavenTemplate/src/main/java/com/arccorp/domain/AccountHelper.java
@@ -0,0 +1,19 @@
+package com.arccorp.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccountHelper {
+	public List<Account> initAccounts() {
+		List<Account> accounts = new ArrayList<Account>();
+		
+		accounts.add(new Account("1", "Steves Account"));
+		accounts.add(new Account("2", "Bobs Account"));
+		accounts.add(new Account("3", "Freds Account"));
+		
+		System.out.println(accounts.size());
+		return accounts;
+		
+		
+	}
+}
