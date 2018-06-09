5c85aa905cf8ba97e360f0b935ea0f61cf433096
steve <steve@kryten>
2018-06-09 15:47:15 -0400

    1

--- MavenTemplate/src/test/java/com/arccorp/test/AccountTest.java
+++ MavenTemplate/src/test/java/com/arccorp/test/AccountTest.java
@@ -1,5 +1,4 @@
 
-
 package com.arccorp.test;
 
 import static org.junit.Assert.assertEquals;
@@ -9,18 +8,16 @@ import org.junit.Test;
 import com.arccorp.domain.Account;
 
 public class AccountTest {
-	  @Test
-	    public void createAccount(){
-		//log.info("Running createAccount test");
-		System.out.print("Running createAccount test, Starts");   
-		    
-		    
-		    
-	        Account a = new Account("123", "Steves Account");
-	        assertEquals("Steves Account was created", true, a.getName().equalsIgnoreCase("Steves Account"));
-		    
-		    System.out.print("Running createAccount test, Ends");   
-	        
-	    }
+	@Test
+	public void createAccount() {
+		// log.info("Running createAccount test");
+		System.out.print("Running createAccount test, Starts");
+
+		Account a = new Account("123", "Steves Account");
+		assertEquals("Steves Account was created", true, a.getName().equalsIgnoreCase("Steves Account"));
+
+		System.out.print("Running createAccount test, Ends");
+
+	}
 
 }
