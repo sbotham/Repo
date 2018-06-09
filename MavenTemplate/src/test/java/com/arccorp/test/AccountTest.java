982c14a194a0554901ecbf710cfe7c89ee91bd38
sbotham <30899578+sbotham@users.noreply.github.com>
2018-06-09 14:03:31 -0400

    Update AccountTest.java

--- MavenTemplate/src/test/java/com/arccorp/test/AccountTest.java
+++ MavenTemplate/src/test/java/com/arccorp/test/AccountTest.java
@@ -1,3 +1,4 @@
+
 package com.arccorp.test;
 
 import static org.junit.Assert.assertEquals;
@@ -9,6 +10,7 @@ import com.arccorp.domain.Account;
 public class AccountTest {
 	  @Test
 	    public void createAccount(){
+		log.info("Running createAccount test");
 	        Account a = new Account("123", "Steves Account");
 	        assertEquals("Steves Account was created", true, a.getName().equalsIgnoreCase("Steves Account"));
 	        
