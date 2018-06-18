d4740ffd59c764a2fa308f5af6109cdf587ec997
Steve Botham <sbotham@arccorp.com>
2018-06-18 09:58:30 -0400

    1

---
+++ MavenTemplate-global/src/main/java/com/arccorp/domain/Account.java
@@ -0,0 +1,13 @@
+package com.arccorp.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account {
+	private String id;
+	private String name;	
+}
---
+++ MavenTemplate-global/src/test/java/com/arccorp/test/AccountTest.java
@@ -0,0 +1,24 @@
+
+package com.arccorp.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.arccorp.domain.Account;
+
+public class AccountTest {
+	@Test
+	public void createAccount() {
+		// log.info("Running createAccount test");
+		System.out.print(" changed on master - 13JUN18 (3.11pm)");
+		System.out.print("Running createAccount test, Starts");
+
+		Account a = new Account("123", "Steves Account");
+		assertEquals("Steves Account was created", true, a.getName().equalsIgnoreCase("Steves Account"));
+
+		System.out.print("Running createAccount test, Ends");
+
+	}
+
+}
